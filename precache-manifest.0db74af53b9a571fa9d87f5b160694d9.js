65b3f1d6e4fe7e60af4f0627ed21a859aec17f19
Marcin Głażewski <marfoczok@gmail.com>
2020-10-02 15:00:33 +0200

    Updates

---
+++ precache-manifest.0db74af53b9a571fa9d87f5b160694d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cbf33430ef476eb0577e912935c226f",
+    "url": "/Marvel-API/index.html"
+  },
+  {
+    "revision": "615757a88a9cb1b787c2",
+    "url": "/Marvel-API/static/css/main.b459ba13.chunk.css"
+  },
+  {
+    "revision": "28ee396d1ba6de7bce93",
+    "url": "/Marvel-API/static/js/2.55e321eb.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/Marvel-API/static/js/2.55e321eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615757a88a9cb1b787c2",
+    "url": "/Marvel-API/static/js/main.73974651.chunk.js"
+  },
+  {
+    "revision": "275afc203a5b67c95d1f",
+    "url": "/Marvel-API/static/js/runtime-main.78df62eb.js"
+  }
+]);
